5074a1d9371d1f646206583099b2e94f5765ad85
mquach <mquach@rossvideo.com>
2025-07-09 14:00:25 -0400

    Implement TagData and return list of TagData

---
+++ backend/TagData.js
@@ -0,0 +1,8 @@
+class TagData {
+    constructor(tagName, startTime, endTime, confidence) {
+        this.tagName = labelName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.confidence = confidence;
+    }
+}
