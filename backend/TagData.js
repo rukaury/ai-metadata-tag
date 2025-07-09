315bbe65d968df7848a31174a1c97949d5344653
mquach <mquach@rossvideo.com>
2025-07-09 14:34:48 -0400

    Update TagData class

--- backend/TagData.js
+++ backend/TagData.js
@@ -1,6 +1,6 @@
-class TagData {
+export default class TagData {
     constructor(tagName, startTime, endTime, confidence) {
-        this.tagName = labelName;
+        this.tagName = tagName;
         this.startTime = startTime;
         this.endTime = endTime;
         this.confidence = confidence;
